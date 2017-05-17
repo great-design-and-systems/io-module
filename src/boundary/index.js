77500dfd1b9a68ddb329d4404d99cf01046c5d34
rickzx98 <rickzx98@gmail.com>
2017-05-17 10:16:02 +0800

    updates

---
+++ src/boundary/index.js
@@ -0,0 +1,6 @@
+
+export default class IOResource {
+    constructor(app) {
+        console.log(app);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { GDSDatabase, GDSServer, GDSServices, GDSUtil } from 'gds-config';
+
+import IOResource from './boundary/';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+new GDSDatabase().connect((errDB) => {
+    if (errDB) {
+        console.error(errDB);
+    } else {
+        new GDSServer(app);
+        new GDSUtil().getLogger(() => {
+            app.listen(PORT, () => {
+                global.gdsLogger.logInfo('Express is listening to port ' + PORT);
+                new IOResource(app);
+            });
+        })
+    }
+});
+
+export default app;
+
+
+
