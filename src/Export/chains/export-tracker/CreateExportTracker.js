8a2fac0695e77e78506da12392ab2c02bffbed64
Analyn Flores <analynflores@gmail.com>
2017-06-22 01:19:25 +0800

    updated export tracker data parameter

--- src/Export/chains/export-tracker/CreateExportTracker.js
+++ src/Export/chains/export-tracker/CreateExportTracker.js
@@ -4,7 +4,15 @@ import { EXPORT_ERROR_HANDLER } from '../util/Chain.info';
 import { ExportTracker } from '../../entity/';
 
 const Action = (context, param, next) => {
-    ExportTracker.create(param.trackerData(), (err, result) => {
+    ExportTracker.create({
+        description: param.description(),
+        status: param.status(),
+        progressCount: param.progressCount(),
+        progressLimit: param.progressLimit(),
+        updatedOn: param.updatedOn(),
+        type: param.type(),
+        fileId: param.fileId()
+    }, (err, result) => {
         context.set('exportTracker', result);
         next(err);
     });
@@ -12,4 +20,10 @@ const Action = (context, param, next) => {
 
 const CreateExportTracker = new Chain(CREATE_EXPORT_TRACKER,
     Action, undefined, EXPORT_ERROR_HANDLER);
-CreateExportTracker.addSpec('trackerData', true);
+CreateExportTracker.addSpec('description', true);
+CreateExportTracker.addSpec('status', true);
+CreateExportTracker.addSpec('progressCount', true);
+CreateExportTracker.addSpec('progressLimit', true);
+CreateExportTracker.addSpec('updatedOn', true);
+CreateExportTracker.addSpec('type', true);
+CreateExportTracker.addSpec('fileId', true);
